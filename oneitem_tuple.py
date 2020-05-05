34ca0e52ce6cf2c0f2344bc83eb755c6e20c6935
nlsai <63520871+nlsai@users.noreply.github.com>
2020-05-06 01:21:15 +0530

    Create oneitem_tuple.py

---
+++ oneitem_tuple.py
@@ -0,0 +1,11 @@
+#My Method
+tup = ('physics', 'chemistry', 1997, 2000)
+print(tup[0:1:])
+
+#Given Solution
+#Create a tuple with numbers
+tuplex = 5, 10, 15, 20, 25
+print(tuplex)
+#Create a tuple of one item
+tuplex = 5,
+print(tuplex)
