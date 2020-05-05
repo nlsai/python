0a58af2b1b344283a2ddd75d2c895c0f8381ed03
nlsai <63520871+nlsai@users.noreply.github.com>
2020-05-06 01:25:07 +0530

    Create add_elements_set.py

---
+++ add_elements_set.py
@@ -0,0 +1,12 @@
+#My Method
+c=set([1,2,3,3,4,4,5])
+c.add(7)
+print(*c,sep="\n")
+
+#Given Solution 
+color_set = set()
+color_set.add("Red")
+print(color_set)
+
+color_set.update(["Blue", "Green"])
+print(color_set)
